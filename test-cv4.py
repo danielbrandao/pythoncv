b0a30b6a021af33d0f9a356c679a9f77d5b138fe
Daniel Brandão <danielbrandao3.0@gmail.com>
2023-09-20 16:04:33 -0300

    Add files via upload

---
+++ test-cv4.py
@@ -0,0 +1,14 @@
+import cv2
+import pytesseract
+from PIL import Image
+import string
+import re
+img2 = cv2.imread("placa-carro.jpg")
+img = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+(T,Thresh1) = cv2.threshold(img, 44, 54, cv2.THRESH_TRUNC)
+(T,Thresh3) = cv2.threshold(Thresh1, 43, 44, cv2.THRESH_BINARY)
+(T,Thresh2) = cv2.threshold(Thresh3, 0 ,255,
+ cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+(T,Thresh4) = cv2.threshold(Thresh2, 30, 255, cv2.CALIB_CB_ADAPTIVE_THRESH)
+cv2.imshow("Imagem 01", Thresh4)
+cv2.waitKey(0)
